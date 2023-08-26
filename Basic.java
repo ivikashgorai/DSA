d58586aa43febb3b025737fc74c60374f528ff40
Vikash Gorai <114097162+vikashgorai@users.noreply.github.com>
2023-08-26 14:10:53 +0530

    commit

---
+++ Basic.java
@@ -0,0 +1,12 @@
+public class Basic{
+    public static void main(String[] args) {
+        print(1); // function to print 1 2 3 4 5
+    }
+    public static void print(int n) {
+        if(n==6){
+            return; // it will return nothing this is called base case  
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}
---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+
+    public static int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
