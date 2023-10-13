c58b910d01031658f8bf0cc65a5844dffab1c320
ivikashgorai <vikashgorai985276@gmail.com>
2023-10-13 15:09:54 +0530

    SumOfDifgitts

---
+++ Recursion/Easy/SumOfDifgitts.java
@@ -0,0 +1,13 @@
+public class SumOfDifgitts {
+    public static void main(String[] args) {
+        int n = 1343;
+        System.out.println(sum(n));
+    }
+
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sum(n / 10);
+    }
+}
