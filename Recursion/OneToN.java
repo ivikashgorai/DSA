cecb0fa3dc540fb9f4ff5c8c55956926838830a2
Vikash <114097162+ivikashgorai@users.noreply.github.com>
2023-10-13 12:46:21 +0530

    Add files via upload

---
+++ Recursion/OneToN.java
@@ -0,0 +1,26 @@
+
+public class OneToN{
+    public static void main(String[] args) {
+        int n=5;
+     one(n);
+     reverse(n);
+    }
+    public static void one(int n) { // 5 4 3 2 1
+        if(n==1){
+            System.out.println(1);
+        }
+        else{
+        System.out.println(n);
+        one(n-1);
+        }
+    }
+        public static void reverse(int n) { // 1 2 3 4 5 
+        if(n==1){
+            System.out.println(1);
+        }
+        else{
+            reverse(n-1);
+        System.out.println(n);
+        }
+    }
+}
