3ab0d3a1d0849658e1253aea12a33c50ac4d84d7
ivikashgorai <vikashgorai985276@gmail.com>
2023-10-17 00:29:24 +0530

    Quick sort

---
+++ Recursion/merge_quick/QuickSort.java
@@ -0,0 +1,37 @@
+package recursion.merge_quick;
+import java.util.Arrays;
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,9,2,1,4,7};
+        quick1(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+    public static void quick1(int[] arr,int low,int high){
+        if(low>=high){
+            return;
+        }
+        int start = low;
+        int end = high;
+        int mid = start + (end-start)/2;
+        int pivot = arr[mid];
+
+        while(start<=end){// if already sorted ,it will not do any action thats why we prefer quick sort over merge sort
+            while(arr[start]<pivot){
+                start++;
+            }
+            while(arr[end]>pivot){
+                end--;
+            }
+            if(start<=end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        quick1(arr,low,end);
+        quick1(arr, start, high);
+    }
+}
