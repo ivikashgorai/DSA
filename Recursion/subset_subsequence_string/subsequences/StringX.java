9229d6bebd3b7b10a3cb6456075d0e5511244b4a
ivikashgorai <vikashgorai985276@gmail.com>
2023-10-19 23:01:00 +0530

    Subsquences

---
+++ Recursion/subset_subsequence_string/subsequences/StringX.java
@@ -0,0 +1,20 @@
+package recursion.subset_subsequence_string.subsequences;
+
+public class StringX {
+    public static void main(String[] args) {
+        String subs = "";
+        String up = "abc";
+        subsequenc(subs,up);
+    }
+    static void subsequenc(String subs,String up){
+        if(up.isEmpty()){
+            System.out.println(subs);
+            return;
+        } 
+        char ch = up.charAt(0); // humko sirf first step sochna hai baki sab apne aap ho jayega
+        // also printing the empty string
+         subsequenc(subs,up.substring(1)); 
+        subsequenc(subs+ch,up.substring(1));
+       // if it is written up then output will from down to up as it printing now
+    }
+}
